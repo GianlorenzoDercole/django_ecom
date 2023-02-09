f4ec072eafafeaf1f0474ee5c3a8aef10515beb3
GianlorenzoDercole <johndercole01@gmail.com>
2023-02-09 14:33:43 -0800

    added checkout button to cart pop over see index.html line 156

--- ecomsite/shop/views.py
+++ ecomsite/shop/views.py
@@ -13,7 +13,7 @@ def index(request):
         product_objects = product_objects.filter(title__icontains=item_name)
 
     # paginator
-    paginator = Paginator(product_objects,2)
+    paginator = Paginator(product_objects,4)
     page = request.GET.get('page')
     product_objects = paginator.get_page(page)
 
