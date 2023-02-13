a9c16a7849d2c125a1528c194328a66121d006ee
GianlorenzoDercole <johndercole01@gmail.com>
2023-02-13 13:50:10 -0800

    added order models to database see models.py line 12 admin.py line 7 and views.py line 27

--- ecomsite/shop/admin.py
+++ ecomsite/shop/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
-from .models import Products
+from .models import Products,Order
 # Register your models here.
 
 # shoe Products model in the admin panel
 admin.site.register(Products)
-
+admin.site.register(Order)
